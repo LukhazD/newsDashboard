10095c9009f57688752793eb5430d2a335f992ae
Luis Diaz <luismanueld.ugas@gmail.com>
2023-07-13 17:06:17 -0400

    fix: * Se coloco la referencia al articulo deseado

--- newsDashboard/src/pages/mainpage/components/MainArticle.tsx
+++ newsDashboard/src/pages/mainpage/components/MainArticle.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { useQuery } from "react-query";
 import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function MainArticle(){
     const [info, setInfo]=useState<IFetchetStockNews | null>(null)
@@ -16,34 +17,36 @@ export default function MainArticle(){
     else{
         try{  return(
         <Stack sx={{flexDirection:'column'}}>
-            <Box sx={{height:'30em', width:match?'44.87em':'100vw'}}>
-                <motion.div
-                initial={{
-                    opacity:match?0:1
-                }}
-                whileHover={{
-                    opacity:1
-                }}
-                >
-                    <Stack sx={{
-                        zIndex:1,
-                        position:'absolute', 
-                        width:match?'44.87em':'100vw', 
-                        height:'30em',
-                        backgroundColor:'rgb(36, 34, 30, 0.4)',
-                        padding:'3em',
-                        justifyContent:'center'
-                        }}>
-                            <Typography sx={{color:'#F7F5F5'}} variant='h2'>
-                                {info?.feed[0].title}
-                            </Typography>
-                    </Stack>
-                </motion.div>
-                <Box 
-                component='img' 
-                src={info?.feed[0].banner_image} 
-                sx={{zIndex:0, height:'30em', width:match?'44.87em':'100vw'}}/>
-            </Box>
+            <Link to={String(info?.feed[0].url)} style={{color:'inherit', textDecoration:'none'}}>
+                <Box sx={{height:'30em', width:match?'44.87em':'100vw'}}>
+                    <motion.div
+                    initial={{
+                        opacity:match?0:1
+                    }}
+                    whileHover={{
+                        opacity:1
+                    }}
+                    >
+                        <Stack sx={{
+                            zIndex:1,
+                            position:'absolute', 
+                            width:match?'44.87em':'100vw', 
+                            height:'30em',
+                            backgroundColor:'rgb(36, 34, 30, 0.4)',
+                            padding:'3em',
+                            justifyContent:'center'
+                            }}>
+                                <Typography sx={{color:'#F7F5F5'}} variant='h2'>
+                                    {info?.feed[0].title}
+                                </Typography>
+                        </Stack>
+                    </motion.div>
+                    <Box 
+                    component='img' 
+                    src={info?.feed[0].banner_image} 
+                    sx={{zIndex:0, height:'30em', width:match?'44.87em':'100vw'}}/>
+                </Box>
+            </Link>
         </Stack>)}
         catch{
             return(<>
--- newsDashboard/src/pages/mainpage/components/SecondaryArticles.tsx
+++ newsDashboard/src/pages/mainpage/components/SecondaryArticles.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion"
 import cutText from "../../../shared/functions/cutText"
 import axios, { AxiosResponse } from "axios"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function SecondaryArticles(){
     const [info, setInfo]=useState<IFetchetStockNews | null>(null)
@@ -26,92 +27,98 @@ export default function SecondaryArticles(){
             justifyContent:'center', 
             gap:match?8:2, 
             width:'100%'}}>
-                <StyledStack>
-                    <motion.p
-                    initial={{
-                        opacity:match?0:1,
-                        backgroundColor:match?'transparent':"rgb(36, 34, 30,0.4)"
-                    }}
-                    whileHover={{
-                        opacity:1,
-                        backgroundColor:'#24221E'
-                    }}
-                    style={{
-                        position:info?'absolute':'unset',
-                        height:'7em', 
-                        width:match ?'12em':'60vw',
-                        color:'#F7F5F5',
-                        display:'flex',
-                        justifyContent:'center',
-                        alignItems:'center',
-                        zIndex:2}}>
-                            {info?.feed[2].title?cutText(String(info?.feed[2].title), 23):<StyledStack/>}
-                    </motion.p>
-                    <Box 
-                    component='img' 
-                    src={info?.feed[2].banner_image} 
-                    sx={{
-                        width:match ?'12em':'60vw', 
-                        height:'100%',
-                        maxHeight:match ?'12em':'60vw'}}/>
-                </StyledStack>
-                <StyledStack>
-                    <motion.p
-                    initial={{
-                        opacity:match?0:1,
-                        backgroundColor:match?'#24221E':"rgb(36, 34, 30,0.4)"
-                    }}
-                    whileHover={{
-                        opacity:1,
-                        backgroundColor:'#24221E'
-                    }}
-                    style={{
-                        position:info?'absolute':'unset',
-                        height:'7em', 
-                        width:'12em',
-                        color:'#F7F5F5',
-                        display:'flex', justifyContent:'center',
-                        alignItems:
-                        'center',
-                        zIndex:2}}>
-                            {info?.feed[3].title!=null?cutText(String(info?.feed[3].title), 23):<StyledStack/>}
-                    </motion.p>
-                    <Box 
-                    component='img' 
-                    src={info?.feed[3].banner_image} 
-                    sx={{
-                        width:'12em', 
-                        height:'100%',
-                        maxHeight:'12em'}}/>
-                </StyledStack>
-                <StyledStack>
-                    <motion.p
-                    initial={{
-                        opacity:match?0:1,
-                        backgroundColor:match?'#24221E':"rgb(36, 34, 30,0.4)"
-                    }}
-                    whileHover={{
-                        opacity:1,
-                        backgroundColor:'#24221E'
-                    }}
-                    style={{
-                        position:info?'absolute':'unset',
-                        height:'7em', 
-                        width:'12em',
-                        color:'#F7F5F5',
-                        display:'flex', justifyContent:'center',
-                        alignItems:'center',
-                        zIndex:2}}>
-                            {info?.feed[8].title?cutText(String(info?.feed[8].title), 23):<StyledStack/>}
-                    </motion.p>
-                    <Box 
-                    component='img' 
-                    src={info?.feed[8].banner_image} 
-                    sx={{
-                        width:'12em', 
-                        height:'100%',
-                        maxHeight:'12em'}}/>
-                </StyledStack>
+                <Link to={String(info?.feed[2].url)} style={{color:'inherit', textDecoration:'none'}}>   
+                    <StyledStack>
+                        <motion.p
+                        initial={{
+                            opacity:match?0:1,
+                            backgroundColor:match?'transparent':"rgb(36, 34, 30,0.4)"
+                        }}
+                        whileHover={{
+                            opacity:1,
+                            backgroundColor:'#24221E'
+                        }}
+                        style={{
+                            position:info?'absolute':'unset',
+                            height:'7em', 
+                            width:match ?'12em':'60vw',
+                            color:'#F7F5F5',
+                            display:'flex',
+                            justifyContent:'center',
+                            alignItems:'center',
+                            zIndex:2}}>
+                                {info?.feed[2].title?cutText(String(info?.feed[2].title), 23):<StyledStack/>}
+                        </motion.p>
+                        <Box 
+                        component='img' 
+                        src={info?.feed[2].banner_image} 
+                        sx={{
+                            width:match ?'12em':'60vw', 
+                            height:'100%',
+                            maxHeight:match ?'12em':'60vw'}}/>
+                    </StyledStack>
+                </Link>
+                <Link to={String(info?.feed[3].url)} style={{color:'inherit', textDecoration:'none'}}>
+                    <StyledStack>
+                        <motion.p
+                        initial={{
+                            opacity:match?0:1,
+                            backgroundColor:match?'#24221E':"rgb(36, 34, 30,0.4)"
+                        }}
+                        whileHover={{
+                            opacity:1,
+                            backgroundColor:'#24221E'
+                        }}
+                        style={{
+                            position:info?'absolute':'unset',
+                            height:'7em', 
+                            width:'12em',
+                            color:'#F7F5F5',
+                            display:'flex', justifyContent:'center',
+                            alignItems:
+                            'center',
+                            zIndex:2}}>
+                                {info?.feed[3].title!=null?cutText(String(info?.feed[3].title), 23):<StyledStack/>}
+                        </motion.p>
+                        <Box 
+                        component='img' 
+                        src={info?.feed[3].banner_image} 
+                        sx={{
+                            width:'12em', 
+                            height:'100%',
+                            maxHeight:'12em'}}/>
+                    </StyledStack>
+                </Link>
+                <Link to={String(info?.feed[8].url)} style={{color:'inherit', textDecoration:'none'}}>
+                    <StyledStack>
+                        <motion.p
+                        initial={{
+                            opacity:match?0:1,
+                            backgroundColor:match?'#24221E':"rgb(36, 34, 30,0.4)"
+                        }}
+                        whileHover={{
+                            opacity:1,
+                            backgroundColor:'#24221E'
+                        }}
+                        style={{
+                            position:info?'absolute':'unset',
+                            height:'7em', 
+                            width:'12em',
+                            color:'#F7F5F5',
+                            display:'flex', justifyContent:'center',
+                            alignItems:'center',
+                            zIndex:2}}>
+                                {info?.feed[8].title?cutText(String(info?.feed[8].title), 23):<StyledStack/>}
+                        </motion.p>
+                        <Box 
+                        component='img' 
+                        src={info?.feed[8].banner_image} 
+                        sx={{
+                            width:'12em', 
+                            height:'100%',
+                            maxHeight:'12em'}}/>
+                    </StyledStack>
+                </Link>
         </Stack>
         </>)}
         catch{
